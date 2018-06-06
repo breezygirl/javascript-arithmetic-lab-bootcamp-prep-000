7f709b011f1454fe27b964ed34bf3eea5dffed93
Beerscheba C <designergo@aol.com>
2018-06-06 11:58:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var a = 10;
+var b = 15;
+function add( a, b ){
+  return a + b;
+}
+
+function subtract(a, b){
+  return a - b;
+}
+function multiply( a, b){
+  return a* b;
+}
+function divide(a, b){
+  return a/b;
+}
+function inc(a){
+  return a+1;
+}
+function dec(a){
+
+  return a -1;
+}
+
+
